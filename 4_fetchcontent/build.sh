9ca11fc28bf6c389a09a7141c4acadd3d3f9117d
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-11-10 21:31:43 +0100

    quick

--- 4_fetchcontent/build.sh
+++ 4_fetchcontent/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 BUILD_DIR=${BUILD_DIR:-build}    # if BUILD_DIR is not set as environment variable, default build directory is "build"
 [ -d $BUILD_DIR ] && rm -rf $BUILD_DIR
-
+[ -d external ] && rm -rf external
 
 # -S .                : sets source directory to the current directory
 # -B build            : sets build directory to the build folder (creates if it does not exist)
